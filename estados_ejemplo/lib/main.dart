005342314b788f5a653797324b13144b4852b763
fatimadelennez <70344884+fatimadelennez@users.noreply.github.com>
2020-10-26 16:42:42 -0600

    ejemplo estados

---
+++ estados_ejemplo/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  String _textValue = '';
+
+  void _updateTextFieldValue(String v){
+    _textValue = v;
+    print(v);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('State on Stateless Widget'),
+        ),
+            body: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                    TextField(
+                      onChanged: (value) => _updateTextFieldValue(value),
+                    ),
+                SizedBox(height: 10.0,),
+                Text(_textValue),
+                ],
+              ),
+      ),
+      ),
+    );
+
+    throw UnimplementedError();
+  }
+
+
+}
+
+
